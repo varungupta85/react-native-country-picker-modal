2da916eb68f423713129db7b17a1d051bf4ffeff
Minu Hariharan <minuhariharan21@gmail.com>
2019-01-15 01:02:07 +0530

    mhariharan: Fix for missing methods

--- src/CountryPicker.style.js
+++ src/CountryPicker.style.js
@@ -1,5 +1,5 @@
 import { StyleSheet, PixelRatio } from 'react-native';
-import { getWidthPercent, getHeightPercent } from './ratio';
+import { getWidthPercent, getHeightPercent, getTop, getBottom } from './ratio';
 
 export default StyleSheet.create({
   modalContainer: {
