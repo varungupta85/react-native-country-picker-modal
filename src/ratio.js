e412b7252865126c2f0a04ab60b7641ceeeb7d4c
Minu Hariharan <minuhariharan21@gmail.com>
2019-01-15 15:50:28 +0530

    mhariharan: Fixed some issues with the missing imports

--- src/ratio.js
+++ src/ratio.js
@@ -1,12 +1,17 @@
-import React from 'react-native';
+import { Dimensions, Platform } from 'react-native'
 
-const { height, width } = React.Dimensions.get('window');
+const { height, width } = Dimensions.get('window')
 
 // Remove the status bar height since the modal view does not cover this area
-const ANDROID_MINUS_HEIGHT = 24;
-const iPHONE_X_MINUS_HEIGHT = 34;
+const ANDROID_MINUS_HEIGHT = 24
+const iPHONE_X_MINUS_HEIGHT = 34
 
-const getHeight = () => (React.Platform.OS === 'android' ? height - ANDROID_MINUS_HEIGHT : (isIphoneX ? height - iPHONE_X_MINUS_HEIGHT : height));
+const getHeight = () =>
+  Platform.OS === 'android'
+    ? height - ANDROID_MINUS_HEIGHT
+    : isIphoneX
+    ? height - iPHONE_X_MINUS_HEIGHT
+    : height
 
 const isIphoneX = () => {
   let dimen = Dimensions.get('window')
@@ -19,8 +24,9 @@ const isIphoneX = () => {
       (dimen.height === 896 || dimen.width === 896))
   )
 }
-export const getWidthPercent = (percentage) => (width * percentage) / 100;
-export const getHeightPercent = (percentage) => (getHeight() * percentage) / 100;
-export const getPercent = (percentage) => (((getHeight() + width) / 2) * percentage) / 100;
-export const getTop = () => (isPhoneX ? 44: 0);
-export const getBottom = () => (isPhoneX ? 34:0)
+export const getWidthPercent = percentage => (width * percentage) / 100
+export const getHeightPercent = percentage => (getHeight() * percentage) / 100
+export const getPercent = percentage =>
+  (((getHeight() + width) / 2) * percentage) / 100
+export const getTop = () => (isIphoneX() ? 44 : 0)
+export const getBottom = () => (isIphoneX() ? 34 : 0)
