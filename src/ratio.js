2cf50e047b83bf4d6a03160e5cee0bc09a991469
Minu Hariharan <minuhariharan21@gmail.com>
2019-01-16 07:35:13 +0530

    mhariharan : Fixed issues with isIphoneX API

--- src/ratio.js
+++ src/ratio.js
@@ -2,17 +2,6 @@ import { Dimensions, Platform } from 'react-native'
 
 const { height, width } = Dimensions.get('window')
 
-// Remove the status bar height since the modal view does not cover this area
-const ANDROID_MINUS_HEIGHT = 24
-const iPHONE_X_MINUS_HEIGHT = 34
-
-const getHeight = () =>
-  Platform.OS === 'android'
-    ? height - ANDROID_MINUS_HEIGHT
-    : isIphoneX
-    ? height - iPHONE_X_MINUS_HEIGHT
-    : height
-
 const isIphoneX = () => {
   let dimen = Dimensions.get('window')
   return (
@@ -24,9 +13,22 @@ const isIphoneX = () => {
       (dimen.height === 896 || dimen.width === 896))
   )
 }
+
+// Remove the status bar height since the modal view does not cover this area
+const ANDROID_MINUS_HEIGHT = 24
+const iPHONE_X_MINUS_HEIGHT = 34
+const iPhoneX = isIphoneX()
+
+const getHeight = () =>
+  Platform.OS === 'android'
+    ? height - ANDROID_MINUS_HEIGHT
+    : iPhoneX
+    ? height - iPHONE_X_MINUS_HEIGHT
+    : height
+
 export const getWidthPercent = percentage => (width * percentage) / 100
 export const getHeightPercent = percentage => (getHeight() * percentage) / 100
 export const getPercent = percentage =>
   (((getHeight() + width) / 2) * percentage) / 100
-export const getTop = () => (isIphoneX() ? 44 : 0)
-export const getBottom = () => (isIphoneX() ? 34 : 0)
+export const getTop = () => (iPhoneX ? 44 : 0)
+export const getBottom = () => (iPhoneX ? 34 : 0)
